ad2ae55fbdd7a4883dd9277b4a1f7c7262522214
Tofu <tsengbatty@gmail.com>
2019-03-21 11:04:03 +0800

    first commit

--- js/lib.js
+++ js/lib.js
@@ -543,20 +543,17 @@ app.getUserOrderList = function(id) {
 			  console.log(title);
 			  
               console.log(order_details.freight);
-              // console.log(order_details.total);
-              // console.log(order_details.payment);
-              // console.log(order_details.shipping);
-              // console.log(order_details.subtotal);
-              // console.log(order_details.recipient);
-              // console.log(order_details.recipient.name);
-              // console.log(order_details.recipient.email);
-              // console.log(order_details.recipient.time);
-              // console.log(order_details.recipient.phone);
-              // console.log(order_details.recipient.address);
+              console.log(order_details.total);
+              console.log(order_details.payment);
+              console.log(order_details.shipping);
+              console.log(order_details.subtotal);
+              console.log(order_details.recipient);
+              console.log(order_details.recipient.name);
+              console.log(order_details.recipient.email);
+              console.log(order_details.recipient.time);
+              console.log(order_details.recipient.phone);
+              console.log(order_details.recipient.address);
             });
-
-            console.log(status);
-
             // render createElement
             // 訂單號碼
             const orderlistID = document.querySelector(".order_id");
